6ec25b7c466bd363ef5a8c833f68a0eba9aa752f
Jaime Martinez Rincon <mrjaime1999@gmail.com>
2016-08-13 20:04:27 +0200

    Hardcoded the repairable items

--- src/main/java/me/jaime29010/crc/Main.java
+++ src/main/java/me/jaime29010/crc/Main.java
@@ -13,7 +13,21 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.Repairable;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.bukkit.Material.*;
+
 public final class Main extends JavaPlugin implements Listener {
+    public static final List<String> REPAIRABLE_ITEMS = Collections.unmodifiableList(Arrays.asList(
+            "WOOD_SWORD", "WOOD_SPADE", "WOOD_HOE", "WOOD_AXE", "WOOD_PICKAXE",
+            "CHAINMAIL_HELMET", "CHAINMAIL_CHESTPLATE", "CHAINMAIL_LEGGINGS", "CHAINMAIL_BOOTS",
+            "IRON_SWORD", "IRON_HELMET", "IRON_CHESTPLATE", "IRON_LEGGINGS", "IRON_BOOTS", "IRON_SPADE", "IRON_HOE", "IRON_AXE", "IRON_PICKAXE",
+            "GOLD_SWORD", "GOLD_HELMET", "GOLD_CHESTPLATE", "GOLD_LEGGINGS", "GOLD_BOOTS", "GOLD_SPADE", "GOLD_HOE", "GOLD_AXE", "GOLD_PICKAXE",
+            "DIAMOND_SWORD", "DIAMOND_HELMET", "DIAMOND_CHESTPLATE", "DIAMOND_LEGGINGS", "DIAMOND_BOOTS", "DIAMOND_SPADE", "DIAMOND_HOE", "DIAMOND_AXE", "DIAMOND_PICKAXE",
+            "SHIELD", "CARROT_STICK", "FLINT_AND_STEEL", "FISHING_ROD", "SHEARS", "BOW"));
     private FileConfiguration config;
     private int cost = 10;
 
@@ -32,7 +46,7 @@ public final class Main extends JavaPlugin implements Listener {
             if (event.getInventory() instanceof AnvilInventory) {
                 ItemStack item = event.getCurrentItem();
                 if (item == null) return;
-                if (item.getItemMeta() instanceof Repairable) {
+                if (REPAIRABLE_ITEMS.contains(item.getType().name())) {
                     Repairable repairable = (Repairable) item.getItemMeta();
                     int current = repairable.getRepairCost();
                     if (current != -1 && current != cost) {
